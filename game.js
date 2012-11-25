e2b737e6a20388c85cb11c51c24901dfaf10c9e4
ryo miyake <ryoujubeat0916@gmail.com>
2012-11-26 02:09:42 +0900

    game.js add code and image add haikei.png

--- game.js
+++ game.js
@@ -1,12 +1,30 @@
+
 enchant();
 
 window.onload = function(){
 	var game = new Game(320,320);
-	game.preload('chara1.gif');
+	game.preload('icon0.gif','images/haikei.png');
+	
+	BOMB = enchant.Class.create(enchant.Sprite,{
+	initialize: function(_x, type){
+		enchant.Sprite.call(this, 16,16);
+		this.x = _x; this.y = 220;
+		this.image = game.assets['icon0.gif'];
+		
+		this.addEventListener(Event.TOUCH_START, function(e){
+		this.remove();
+		});
+		game.rootScene.addChild(this);
+		},
+		remove: function () {
+			game.rootScene.removeChild(this);
+			}
+		});
 	game.onload = function(){
-	var sprite = new Sprite(32,32);
-	sprite.image = game.assets['chara1.gif'];
-	game.rootScene.addChild(sprite);
+	var scene = game.rootScene;
+	var bg = new Sprite (320,320);
+	bg.image = game.assets['images/haikei.png'];
+	game.rootScene.addChild(bg);
  };
 	game.start();
 };
